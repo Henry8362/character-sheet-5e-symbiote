19fdc0975ba438f5801bc5d6532d37ec6be0b259
Henry Evans <33024428+Henry8362@users.noreply.github.com>
2023-08-21 20:55:20 +0100

    commits

--- brains.js
+++ brains.js
@@ -1,7 +1,7 @@
 // This file will be loaded into the browser along with the index.html file and make the character sheets work.
 // It will contact an external service and make a fetch request to pull in a JSON array of data for the character sheet, which will then load into the page.
 // Called when page is loaded
-async function initSheet() {
+async function initSheet(e) {
 
   // Get the player characters associated with player who has the symbiote, this will be by the unique ID of the model.
   //let $playercharacters = TS.creatures.getCreaturesOwnedByPlayer;
@@ -9,7 +9,10 @@ async function initSheet() {
   //$playercharacters.forEach(function (character) {
     // get the ID if unique
    // if (character.isUnique) {
-    let id = 1;
+    console.log(e);
+    let id = e.id;
+
+    //let id = "782831a7-83d4-49a9-92e6-b61646592d24";
     // make fetch request to get the character sheet data
     var response = await fetch(`http://localhost:3000/Players/${id}`)
     var data = await response.json();
@@ -44,3 +47,144 @@ async function fetchBlocks() {
       return data;
       }
   }
+
+async function fetchItems() {
+
+  var response = await fetch(`http://localhost:3000/Items`)
+  var data = await response.json();
+  return data;
+
+}
+
+
+async function  getPlayers() {
+
+  players = await TS.players.getPlayersInThisCampaign();
+  return players;
+  //await TS.creatures.getUniqueCreaturesInThisCampaign();
+
+}
+
+async function getUser() {
+
+  player = await TS.players.whoAmI();
+  return player;
+
+
+}
+
+async function getUsersCreatures(e) {
+
+  playersCreatures = await TS.creatures.getUniqueCreaturesInThisCampaign();
+  playersCreatures = await TS.creatures.getMoreInfo(playersCreatures);
+  playersCreatures = playersCreatures.filter(creature => creature.ownerIds.includes(e));
+  return playersCreatures;
+
+}
+
+async function getUsersResources(e) {
+
+  // for the given player, we make an API query to get the resources that player has access to.
+  // we then return that data to the calling function.
+  console.log(e);
+  var response = await fetch(`http://localhost:3000/Players/${e}/Resources`);
+  var data = await response.json();
+  return data;
+}
+
+async function createSheet(id, data) {
+  console.log(data);
+  var response = await fetch(`http://localhost:3000/Players/${id}/MakeSheet`, {
+    method: 'POST', // Use 'PUT' if updating instead of creating
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+
+
+});
+var data = await response.json();
+return data;
+};
+
+async function addItemToPlayerSheet(itemID, CharacterID) {
+
+  var response = await fetch(`http://localhost:3000/Characters/${CharacterID}/Items/${itemID}`, {
+    method: 'POST', // Use 'PUT' if updating instead of creating
+    headers: {
+      'Content-Type': 'application/json'
+    },
+
+  });
+
+
+}
+
+async function deductItemFromPlayerSheet(itemID, CharacterID, quantity) {
+
+  var response = await fetch(`http://localhost:3000/Characters/${CharacterID}/Items/${itemID}/deduct/${quantity}`, {
+    method: 'POST', // Use 'PUT' if updating instead of creating
+    headers: {
+      'Content-Type': 'application/json'
+    },
+
+
+});
+
+}
+
+async function handleRollResult(rollEvent) {
+
+    let result = rollEvent.payload.resultsGroups[0].result.results;
+    // add up the total result.results 
+    let total = 0;
+    // Doesn't account for if a singular dice is rolled at the moment, need to fix that.
+    if( result != undefined) {
+      // add up the array values of result
+      result.forEach(function (item) {
+        total += item;
+      }
+      );
+
+    }
+    let data = VueApp.rollData;
+    console.log(data);
+    if (rollEvent.payload.rollId == data[0].rollID) {
+        //if the roll event matches the roll id of the roll we are waiting for, we can process the result.
+        
+        // if it is a consumable
+        if (data[0].type == "item") {
+          // determine the attribute type 
+          switch(data[0].item.attributes.type) {
+            case "heal":
+              let bonusHealing = data[0].item.attributes.bonus;
+              totalHealing = parseInt(total) + parseInt(bonusHealing);
+              // add the total healing to the character sheet via the API and deduct the item from the character sheet.
+              // we need to get the character sheet ID from the roll data.
+              let characterID = data[0].character_id;
+              let itemID = data[0].item.id;
+              // add the item to the character sheet
+              await deductItemFromPlayerSheet(itemID, characterID, 1);
+              // add the healing to the character sheet
+              // await addHealingToPlayerSheet(characterID, totalHealing);
+
+            break;
+
+
+
+
+          }
+
+
+        }
+        else if (data[0].type == "attack") {
+
+        }
+        //unset VueApp.rollData
+        VueApp.rollData = [];
+        return;
+    }
+  
+  
+
+}
